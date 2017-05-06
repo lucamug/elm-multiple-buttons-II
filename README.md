14bacca41bd4fc06e02aa2732d59edf5b5f342dc
Luca <lucamug@gmail.com>
2017-05-06 11:11:10 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+凸凹凸凹凸凹凸凹凸凹凸凹凸凹凸凹凸凹凸凹凸
+
+Counters Bonanza
+
+Experimenting with recycling Elm code transforming it in a module
+
+I took the official "buttons" example (http://elm-lang.org/examples/buttons) and loaded from another module.
+
+To see the code:
+
+    $ git clone https://github.com/lucamug/elm-multiple-buttons-II.git
+    $ cd elm-multiple-buttons/
+    $ elm-reactor
+
+and open
+
+ - http://localhost:8000/source/buttons.elm
+ - http://localhost:8000/source/buttonslist.elm
+ - http://localhost:8000/source/main.elm
+
+The imported module can also be execute independently.
---
+++ source/main.elm
@@ -0,0 +1,101 @@
+module Main exposing (view, Msg, update)
+
+import Buttonslist as Imported
+import Html exposing (beginnerProgram, div, button, text, pre)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
+import Array
+
+
+defaultElementIfNothing =
+    []
+
+
+main =
+    beginnerProgram
+        { model = []
+        , view = view
+        , update = update
+        }
+
+
+view model =
+    div [ style [ ( "margin", "10px" ) ] ]
+        [ button [ onClick (Add) ] [ text "Add" ]
+        , div []
+            (List.indexedMap
+                (\index element ->
+                    div
+                        [ style
+                            [ ( "border", "1px solid #aaa" )
+                            , ( "background-color", "#eee" )
+                            , ( "display", "inline-block" )
+                            , ( "padding", "10px" )
+                            , ( "margin", "10px" )
+                            ]
+                        ]
+                        [ Html.map
+                            (MsgFromChild index)
+                            (div
+                                []
+                                [ Imported.view element ]
+                            )
+                        , button [ onClick (Remove index) ] [ text "Remove" ]
+                        ]
+                )
+                model
+            )
+        , pre [] [ text ("model: " ++ (toString model)) ]
+        ]
+
+
+type Msg
+    = MsgFromChild Int Imported.Msg
+    | Remove Int
+    | Add
+
+
+getElementFromList list index default =
+    list
+        |> Array.fromList
+        |> Array.get index
+        |> replaceMaybeWithDefault default
+
+
+replaceMaybeWithDefault default element =
+    case element of
+        Nothing ->
+            default
+
+        Just val ->
+            val
+
+
+setElementIntoList list index element =
+    list
+        |> Array.fromList
+        |> Array.set index element
+        |> Array.toList
+
+
+removeElementFromList index list =
+    (List.take index list) ++ (List.drop (index + 1) list)
+
+
+update msg model =
+    case msg of
+        MsgFromChild index msgFromChild ->
+            let
+                oldElement =
+                    getElementFromList model index defaultElementIfNothing
+
+                newElement =
+                    Imported.update msgFromChild oldElement
+            in
+                setElementIntoList model index newElement
+
+        Remove index ->
+            removeElementFromList index model
+
+        Add ->
+            List.append model [ defaultElementIfNothing ]
